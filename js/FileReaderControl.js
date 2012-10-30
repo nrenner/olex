882b3b9b7405be7324f0829b3edb2a8755f3cfc4
Norbert Renner <norbert.renner@gmx.de>
2012-10-30 21:57:04 +0100

    fixed url regex + cleanup

--- js/FileReaderControl.js
+++ js/FileReaderControl.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Reads files using HTML5 file API
  *  
  * derived from http://www.html5rocks.com/en/tutorials/file/dndfiles/
@@ -11,7 +11,6 @@ FileReaderControl.prototype.activate = function() {
     if (window.File && window.FileReader && window.FileList) {
         document.getElementById('fileinput').addEventListener('change', _.bind(this.handleFileSelect, this), false);
 
-        // TODO map.div
         var dropZone = document.getElementById('map_div'); 
         dropZone.addEventListener('dragover', this.handleDragOver, false);
         dropZone.addEventListener('drop', _.bind(this.handleFileSelect, this), false);
@@ -21,22 +20,6 @@ FileReaderControl.prototype.activate = function() {
         console.warn('Browser does not support the HTML5 File API!');
     }    
 };
-
-/*
-FileReaderControl.prototype.handleLoad = function(text, fileName) {
-    var features = this.formatManager.read(text);
-    changes.addFeatures(features);
-    map.zoomToExtent(changes.getDataExtent());
-
-    / *
-    if (text.match(/<osmChange\s/)) {
-        var features = oscFormat.read(text);
-    } else {
-        console.error("File content not recognized as osmChange (OSC): " + fileName);
-    } 
-    * /
-};
-*/
 
 FileReaderControl.prototype.handleFileSelect = function(evt) {
     var files, file;
@@ -55,7 +38,6 @@ FileReaderControl.prototype.handleFileSelect = function(evt) {
             var handleLoad = _.bind(this.onLoadCallback, this);
             fileReader.onload = function(evt) {
                 var text = evt.target.result;
-                //handleLoad(text, file.name);
                 handleLoad(text, file.name);
             };
             fileReader.onerror = function(evt) {
@@ -68,25 +50,9 @@ FileReaderControl.prototype.handleFileSelect = function(evt) {
     } else if (evt.dataTransfer ) {
         var url = evt.dataTransfer.getData("URL");
         if (url) {
-            /*
-            var xhr = new XMLHttpRequest();
-            url = '/cgi-bin/proxy.cgi?url=' + url;
-            xhr.open('GET', url, false);
-            xhr.send();
-            console.log('status = ' + xhr.status);
-            this.onLoadCallback(xhr.responseText, url);
-            */
-			
-            // TODO document why proxy (Overpass: no proxy needed (CORS, but x-requested-with Request patch), used for gzip headers)
-            //url = '/cgi-bin/proxy.cgi?url=' + url;
-			
-            // TODO support multiple, error when URL unknown
-			// http://overpass-api.de/augmented_diffs/000/008/066.osc.gz
-			//var regex = /http:\/\/overpass-api.de\/augmented_diffs\/([0-9]{3})\/([0-9]{3})\/([0-9]{3}).osc.gz/g;
-			// http://overpass-api.de/augmented_diffs/id_sorted/000/028/706.osc.gz
-            var regex = /http:\/\/overpass-api.de\/augmented_diffs\/id_sorted\/([0-9]{3})\/([0-9]{3})\/([0-9]{3}).osc.gz/g;
-			//var match = myRegexp.exec(myString);
-			//alert(match[1]);
+			// http://www.overpass-api.de/augmented_diffs/000/008/066.osc.gz
+			// http://www.overpass-api.de/augmented_diffs/id_sorted/000/028/706.osc.gz
+            var regex = /.*overpass-api\.de\/augmented_diffs(?:\/id_sorted|)\/([0-9]{3})\/([0-9]{3})\/([0-9]{3}).osc.gz/;
 			var sequence = parseFloat(url.replace(regex, "$1$2$3"));
 			url = 'http://overpass-api.de/api/augmented_diff?id=' + sequence;
 			console.log("requesting " + url);
